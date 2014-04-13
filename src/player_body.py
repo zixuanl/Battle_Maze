debacdb1d63192b96e2b08b625d8159692beee22
Arvind Balaji <barvind89@gmail.com>
2014-04-13 15:01:03 -0400

    use level2 for testing

--- src/player_body.py
+++ src/player_body.py
@@ -31,41 +31,6 @@ I_WIN = "IWIN"
 I_LOST="LOST"
 INFORM_GAME_END_BOOTSTRAP="OVER"
 
-class Vector():
-    '''
-        Class:
-            creates operations to handle vectors such
-            as direction, position, and speed
-        '''
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def __str__(self): # used for printing vectors
-        return "(%s, %s)"%(self.x, self.y)
-
-    def __getitem__(self, key):
-        if key == 0:
-            return self.x
-        elif key == 1:
-            return self.y
-        else:
-            raise IndexError("This "+str(key)+" key is not a vector key!")
-
-    def __sub__(self, o): # subtraction
-        return Vector(self.x - o.x, self.y - o.y)
-
-    def length(self): # get length (used for normalize)
-        return math.sqrt((self.x**2 + self.y**2)) 
-
-    def normalize(self): # divides a vector by its length
-        l = self.length()
-        if l != 0:
-            return (self.x / l, self.y / l)
-        return None
-
-
-
 class player(pygame.sprite.Sprite,Communicate):
     game_over='false'
     def __init__(self, location,player_num,*groups):
